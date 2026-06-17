fd57e35b2f961d674664147eb60f075429f38b0e
AbeTavarez <abeincwireless@gmail.com>
2026-06-17 00:45:42 -0400

    graph nodes created

--- src/secureagent/agents/graph.py
+++ src/secureagent/agents/graph.py
@@ -1,14 +1,14 @@
 from langgraph.graph import StateGraph, START, END
-from langgraph.prebuilt import ToolNode, tools_condition
 from langchain.messages import HumanMessage
 from typing import cast
 
 from secureagent.agents.state import AgentState
-from secureagent.agents.nodes import llm_call, tool_node
+from secureagent.agents.nodes import llm_call, tool_node, should_continue
 
 from IPython.display import Image, display
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -18,28 +18,28 @@ agent_workflow = StateGraph(AgentState)
 
 # ===== Nodes =====
 agent_workflow.add_node("llm_call", llm_call)
-agent_workflow.add_node("tools", tool_node)
+agent_workflow.add_node("tool_node", tool_node)
+
 
 
 # ==== Edges =====
 agent_workflow.add_edge(START, "llm_call")
-agent_workflow.add_conditional_edges("llm_call", tools_condition)
-agent_workflow.add_edge("tools", "llm_call")
-agent_workflow.add_edge("llm_call", END)
+agent_workflow.add_conditional_edges("llm_call", should_continue, ["tool_node", END])
+agent_workflow.add_edge("tool_node", "llm_call")
 
 
 # Compile the graph
 agent = agent_workflow.compile()
 
 
-# Visualize Graph 
+# Visualize Graph
 def get_graph_diagram():
     try:
         display(Image(agent.get_graph().draw_mermaid_png()))
     except Exception:
         print("Additional dependencies required.")
 
-
+#! Remove after creating CLI
 if __name__ == "__main__":
 
     user_prompt = input("Enter prompt: ")
--- src/secureagent/agents/nodes.py
+++ src/secureagent/agents/nodes.py
@@ -1,7 +1,8 @@
 from langchain_groq import ChatGroq
 from langchain.messages import SystemMessage
 from langfuse.langchain import CallbackHandler
-from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.graph import END
+from langchain.messages import AIMessage, ToolMessage
 from secureagent.agents.state import AgentState
 
 # Tools
@@ -15,8 +16,9 @@ langfuse_handler = CallbackHandler()
 
 # Model Tools
 tools = [get_health_status]
-tool_node = ToolNode(tools)
+tools_by_name = {tool.name: tool for tool in tools}
 
+# Model
 model = ChatGroq(model="qwen/qwen3-32b", temperature=0)
 model_with_tools = model.bind_tools(tools)
 
@@ -35,3 +37,30 @@ def llm_call(state: AgentState):
             )
         ],
     }
+
+
+def tool_node(state: AgentState):
+    
+    result = []
+    last_message = state["messages"][-1]
+    
+    # Only AIMessages have tool_calls
+    if isinstance(last_message, AIMessage) and last_message.tool_calls:
+        for tool_call in last_message.tool_calls:
+            tool = tools_by_name[tool_call["name"]]
+            observation = tool.invoke(tool_call["args"])
+            result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+    return {"messages": result}
+
+
+def should_continue(state: AgentState):
+    """Decide if we should continue the loop or stop based upon whether the LLM made a tool call"""
+
+    last_message = state["messages"][-1]
+
+    # If the LLM makes a tool call, then perform an action
+    if isinstance(last_message, AIMessage) and last_message.tool_calls:
+        return "tool_node"
+
+    # Otherwise, we stop (reply to the user)
+    return END
